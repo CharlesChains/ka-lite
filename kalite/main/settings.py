8635c0beb4a4bb9815fc1e0edef272cb446ead75
Carlos Cadenas <CCadenas@belatrixsf.com>
2017-07-04 11:42:59 -0300

    SAML Implementation work in progress

--- kalite/main/settings.py
+++ kalite/main/settings.py
@@ -6,3 +6,8 @@
 #   NOTE: None means no limit (infinite)
 USER_LOG_MAX_RECORDS_PER_USER = 1
 USER_LOG_SUMMARY_FREQUENCY = (1, "day")
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SAML_FOLDER = os.path.join(BASE_DIR, 'saml')
+LOGIN_URL = '/sp/sso/login/'
---
+++ refresh.sh
@@ -0,0 +1,4 @@
+kalite stop
+python setup.py install
+pip install -e .
+kalite manage setup
